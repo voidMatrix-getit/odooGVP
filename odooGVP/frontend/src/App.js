836c6865e2cbb236b03e8720a378ea3197d15542
sahil <114466658+voidMatrix-getit@users.noreply.github.com>
2024-07-12 20:50:06 +0530

    fe only one error

--- odooGVP/frontend/src/App.js
+++ odooGVP/frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './styles/tailwind.css';
-import Header from './components/Header'
+import Header from './components/Header';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Drawer from './components/Drawer';
